8fbeb81eed6176c13a37ad81af923807e295df2a
Monther5 <monder_4207@limu.edu.ly>
2025-12-03 18:12:08 +0200

    feat(auth): create admin login controller and routes

--- routes/web.php
+++ routes/web.php
@@ -7,56 +7,65 @@ use App\Http\Controllers\Admin\BookController;
 use App\Http\Controllers\Admin\CartController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\AuthController;
 
 Route::get('/', function () {
     return redirect()->route('admin.dashboard');
 });
 
 Route::prefix('admin')->name('admin.')->group(function () {
-    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    // Auth Routes
+    Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('login', [AuthController::class, 'login'])->name('login.post');
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 
-    Route::resource('classifications', ClassificationController::class)->names([
+    // Protected Routes
+    Route::middleware('auth:admin')->group(function () {
+        Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-        'index' => 'classifications.index',
-        'create' => 'classifications.create',
-        'store' => 'classifications.store',
-        'show' => 'classifications.show',
-        'edit' => 'classifications.edit',
-        'update' => 'classifications.update',
-        'destroy' => 'classifications.destroy',
-    ]);
+        Route::resource('classifications', ClassificationController::class)->names([
 
-    Route::resource('types', TypeController::class)->names([
-        'index' => 'types.index',
-        'create' => 'types.create',
-        'store' => 'types.store',
-        'edit' => 'types.edit',
-        'update' => 'types.update',
-        'destroy' => 'types.destroy',
-    ]);
+            'index' => 'classifications.index',
+            'create' => 'classifications.create',
+            'store' => 'classifications.store',
+            'show' => 'classifications.show',
+            'edit' => 'classifications.edit',
+            'update' => 'classifications.update',
+            'destroy' => 'classifications.destroy',
+        ]);
 
-    Route::resource('books', BookController::class)->names([
-        'index' => 'books.index',
-        'create' => 'books.create',
-        'store' => 'books.store',
-        'edit' => 'books.edit',
-        'update' => 'books.update',
-        'destroy' => 'books.destroy',
-    ]);
+        Route::resource('types', TypeController::class)->names([
+            'index' => 'types.index',
+            'create' => 'types.create',
+            'store' => 'types.store',
+            'edit' => 'types.edit',
+            'update' => 'types.update',
+            'destroy' => 'types.destroy',
+        ]);
 
-    Route::resource('carts', CartController::class)->only([
-        'index', 'destroy'
-    ])->names([
-        'index' => 'carts.index',
-        'destroy' => 'carts.destroy',
-    ]);
+        Route::resource('books', BookController::class)->names([
+            'index' => 'books.index',
+            'create' => 'books.create',
+            'store' => 'books.store',
+            'edit' => 'books.edit',
+            'update' => 'books.update',
+            'destroy' => 'books.destroy',
+        ]);
 
-    Route::resource('categories', CategoryController::class)->names([
-        'index' => 'categories.index',
-        'create' => 'categories.create',
-        'store' => 'categories.store',
-        'edit' => 'categories.edit',
-        'update' => 'categories.update',
-        'destroy' => 'categories.destroy',
-    ]);
+        // Route::resource('carts', CartController::class)->only([
+        //     'index', 'destroy'
+        // ])->names([
+        //     'index' => 'carts.index',
+        //     'destroy' => 'carts.destroy',
+        // ]);
+
+        Route::resource('categories', CategoryController::class)->names([
+            'index' => 'categories.index',
+            'create' => 'categories.create',
+            'store' => 'categories.store',
+            'edit' => 'categories.edit',
+            'update' => 'categories.update',
+            'destroy' => 'categories.destroy',
+        ]);
+    });
 });
